f177e1292d44e95627aa34fd5e7e5cd8d611a2d2
Nuwan <nuwan_g@live.com>
2019-08-07 13:16:08 +0530

    Init commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,23 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import YouTube from "react-youtube"
 
 import Header from "./header"
 import "./layout.css"
+
+const videoOptions = {
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+    controls: 0,
+    rel: 0,
+    showinfo: 0,
+  },
+}
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,6 +34,23 @@ const Layout = ({ children }) => {
     }
   `)
 
+  const _onReady = event => {
+    // access to player in all event handlers via event.target
+    // event.target.mute();
+    event.target.playVideo()
+
+    setTimeout(() => {
+      setPlayer(true)
+      event.target.stopVideo()
+    }, 13000)
+  }
+
+  const _onEnd = event => {
+    event.target.playVideo()
+  }
+
+  const [hidePlayer, setPlayer] = useState(false)
+
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
@@ -32,15 +60,24 @@ const Layout = ({ children }) => {
           maxWidth: 960,
           padding: `0px 1.0875rem 1.45rem`,
           paddingTop: 0,
+          display: hidePlayer ? "none" : "block",
+          zIndex: hidePlayer ? -99 : 1024,
         }}
       >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <div className="video-background">
+          <div className="video-foreground">
+            <YouTube
+              videoId="_mrdWNcoLYo"
+              opts={videoOptions}
+              className="video-iframe"
+              onReady={_onReady}
+              onEnd={_onEnd}
+            />
+          </div>
+        </div>
       </div>
+      <main style={{maxWidth: "80vw", margin: "0 auto", display: "flex", justifyContent:"center", alignItems:"center", height: "80vh"}}>{children}</main>
+      <footer style={{position: "fixed", bottom: "0", margin:"20px"}}>© {new Date().getFullYear()}, Built by Nugoo</footer>
     </>
   )
 }
